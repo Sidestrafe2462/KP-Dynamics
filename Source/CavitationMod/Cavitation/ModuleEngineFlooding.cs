c038f1094dc07aa65517e70a11093beb745ec3a1
Spartwo <sparthesecond@gmail.com>
2025-03-11 01:27:18 +0000

    Added Engine Flooding
    Normal Airbreathing engines in the water now flameout

---
+++ Source/CavitationMod/Cavitation/ModuleEngineFlooding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using System.Reflection;
+using VehiclePhysics;
+
+namespace Caviation
+{
+    public class ModuleEngineFlooding : PartModule
+    {
+        // This class references the stock engine module to turn on the unused squad engine in water check
+
+        private ModuleEngines engineModule;
+        private ModuleEnginesFX engineFXModule;
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+
+            // Find an engine module on the same part
+            engineModule = part.FindModuleImplementing<ModuleEngines>();
+            engineFXModule = part.FindModuleImplementing<ModuleEnginesFX>();
+
+            if (engineModule == null && engineFXModule == null)
+            {
+                Debug.LogError($"[KPDynamics] No compatible engine module found on this {part.name}!");
+            }
+
+            if (engineModule != null) engineModule.disableUnderwater = true;
+            if (engineFXModule != null) engineFXModule.disableUnderwater = true;
+        }
+    }
+}
